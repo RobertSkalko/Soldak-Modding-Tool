76185f1c7aaaacf6e068af7d6120b85fd041fc35
robert trebor <treborx555@gmail.com>
2018-09-30 22:52:50 +0200

    Added scene selector, everything seems to work but i bet ill find more bugs

---
+++ Assets/Scripts/Monobehaviors/LoadSceneSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SoldakModdingTool
+{
+    public class LoadSceneSelector : MonoBehaviour
+    {
+        private void Start()
+        {
+            Button butt = this.gameObject.GetComponent<Button>();
+
+            butt.onClick.AddListener(() => {
+                UnityEngine.SceneManagement.SceneManager.LoadScene("SceneSelector");
+            });
+        }
+    }
+}
